c7f276cbf6bcd0d1fdb04d7bd884040e20646e79
zhaojian770627 <zhaojian770627@163.com>
2017-06-15 22:56:12 +0800

    add

--- protected/9/c09.asm
+++ protected/9/c09.asm
@@ -17,10 +17,99 @@ header_end:
 
 	;; ================================================================
 section code_1 align=16 vstart=0 ;定义代码段1(16字节对齐)
+new_int_0x70:
+	push	ax
+	push	bx
+	push	cx
+	push	dx
+	push	es
+.w0:
+	mov	al,0x0a		;阻断NMI。当然，通常不必要的
+	or	al,0x80
+	out	0x70,al
+	in	al,0x71		;读寄存器A
+	test	al,0x80		;测试第7位UIP
+	jnz	.w0		;以上代码对于更新周期结束中断来说是不必要的
 
-;;;================================================================
+	xor	al,al
+	or	al,0x80
+	out	0x70,al
+	in	al,0x71		;读RTC秒
+	push	ax
+
+	mov	al,2
+	or	al,0x80
+	out	0x70,al
+	in	al,0x71		;读分
+	push	ax
+
+	mov	al,4
+	or	al,0x80
+	out	0x70,al
+	in	al,0x71		;时
+	push	ax
+
+	mov	al,0x0c		;寄存器C的索引.且开放NMI
+	out	0x70,al
+	in	al,0x71		;读下RTC的寄存器C,否则只发生一次中断，此处不考虑闹钟和周期性中断的情况
+
+	mov	ax,0xb800
+	mov	es,ax
+
+	pop	ax
+	call	bcd_to_assii
+	mov	bx,12*168+36*2	;从屏幕的12行36列开始显示
+
+	mov	[es:bx],ah
+	mov	[es:bx+2],al	;显示两位小时数字
+
+	mov	al,':'
+	mov	[es:bx+4],al	;显示分割符
+	not	byte[es:bx+5]	;反转显示属性
+
+	pop	ax
+	call 	bcd_to_assii
+	mov	[es:bx+6],ah
+	mov	[es:bx+8],al	;显示两位分钟数字
+
+	mov	al,':'
+	mov	[es:bx+10],al	;显示分割符
+	not 	byte[es:bx+11]	;反转显示属性
+
+	pop	ax
+	call 	bcd_to_assii
+	mov	[es:bx+12],ah
+	mov	[es:bx+14],al	;显示两位秒数字
+
+	mov	al,0x20		;中断结束命令
+	out	0xa0,al		;向从片发送
+	out	0x20,al		;向主片发送
+	
+	pop	es
+	pop	dx
+	pop	cx
+	pop	bx
+	pop	ax
+
+	iret
+;;; ---------------------------------------------------------------
+	;; BCD妈转ASCII
+	;; Input:AL=bcd妈
+	;; Output:AX=ascii
+bcd_to_ascii:
+	mov	ah,al		;分解成两个数字
+	and	al,0x0f		;仅保留低4位
+	add	al,0x30		;转换成ASCII
+
+	shr	ah,4		;逻辑右移4位
+	and 	ah,0x0f
+	add	ah,0x30
+
+	ret
+		
+;;;--------------------------------------------------------------
 start:
-	halt
+	hlt
 ;;; ==========================================================================
 section data_1 align=16 vstart=0
 
