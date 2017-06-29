0c0b6d55cfa03215e96d8ac99f2af99e753575ca
zhaojian770627 <zhaojian770627@163.com>
2017-06-29 22:08:30 +0800

    add

--- tty/menu.c
+++ tty/menu.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 char *menu[]={
   "a - add new record",
@@ -7,20 +9,34 @@ char *menu[]={
   NULL
 };
 
-int getchoice(char *greet,char *choices[]);
+int getchoice(char *greet,char *choices[],FILE *in,FILE *out);
 
 int main()
 {
   int choice=0;
+  FILE *input;
+  FILE *output;
+
+  if(!isatty(fileno(stdout))){
+    fprintf(stderr,"You are not a terminal.OK.\n");
+  }
+
+  input=fopen("/dev/tty","r");
+  output=fopen("/dev/tty","w");
+
+  if(!input || !output){
+    fprintf(stderr,"Unable to open /dev/tty\n");
+    exit(1);
+  }
 
   do{
-    choice=getchoice("Please select an action",menu);
+    choice=getchoice("Please select an action",menu,input,output);
     printf("You have chosen:%c\n",choice);
   }while(choice!='q');
   return 0;
 }
 
-int getchoice(char *greet,char *choices[])
+int getchoice(char *greet,char *choices[],FILE *in,FILE *out)
 {
   int chosen=0;
   int selected;
@@ -30,10 +46,12 @@ int getchoice(char *greet,char *choices[])
     printf("Choice:%s\n",greet);
     option=choices;
     while(*option){
-      printf("%s\n",*option);
+      fprintf(out,"%s\n",*option);
       option++;
     }
-    selected=getchar();
+    do{
+      selected=fgetc(in);
+    }while(selected=='\n');
     option=choices;
     while(*option){
       if(selected==*option[0]){
@@ -43,7 +61,7 @@ int getchoice(char *greet,char *choices[])
       option++;
     }     
     if(!chosen){
-      printf("Incorrect choice,select again\n");
+      fprintf(out,"Incorrect choice,select again\n");
     }
   }while(!chosen);
   return selected;
