13042c65add8b1dd9f141454821365e882ad5a0a
zhaojian770627 <zhaojian770627@163.com>
2017-07-01 19:47:24 +0800

    up

--- tty/menu.c
+++ tty/menu.c
@@ -2,6 +2,10 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <termios.h>
+#include <term.h>
+#include <curses.h>
+
+static FILE *output_stream=(FILE*)0;
 
 char *menu[]={
   "a - add new record",
@@ -11,6 +15,7 @@ char *menu[]={
 };
 
 int getchoice(char *greet,char *choices[],FILE *in,FILE *out);
+int char_to_terminal(int char_to_write);
 
 int main()
 {
@@ -37,6 +42,7 @@ int main()
   new_settings.c_lflag &=~ECHO;
   new_settings.c_cc[VMIN]=1;
   new_settings.c_cc[VTIME]=0;
+  new_settings.c_lflag &=~ISIG;
   if(tcsetattr(fileno(input),TCSANOW,&new_settings)!=0){
     fprintf(stderr,"could not set attributes\n");
   }
@@ -53,18 +59,32 @@ int getchoice(char *greet,char *choices[],FILE *in,FILE *out)
 {
   int chosen=0;
   int selected;
+  int screenrow,screencol=10;
+
   char **option;
+  char *cursor,*clear;
+
+  output_stream=out;
+
+  setupterm(NULL,fileno(out),(int*)0);
+  cursor=tigetstr("cup");
+  clear=tigetstr("clear");
+
+  screenrow=4;
+  tputs(clear,1,char_to_terminal);
+  tputs(tparm(cursor,screenrow,screencol),1,char_to_terminal);
+  fprintf(out,"Choice:%s",greet);
+  screenrow+=2;
+  option=choices;
+  while(*option){
+    tputs(tparm(cursor,screenrow,screencol),1,char_to_terminal);
+    fprintf(out,"%s",*option);
+    screenrow++;
+    option++;
+  }
 
   do{
-    printf("Choice:%s\n",greet);
-    option=choices;
-    while(*option){
-      fprintf(out,"%s\n",*option);
-      option++;
-    }
-    do{
-      selected=fgetc(in);
-    }while(selected=='\n');
+    selected=fgetc(in);
     option=choices;
     while(*option){
       if(selected==*option[0]){
@@ -72,10 +92,19 @@ int getchoice(char *greet,char *choices[],FILE *in,FILE *out)
 	break;
       }
       option++;
-    }     
+    }
     if(!chosen){
-      fprintf(out,"Incorrect choice,select again\n");
+      tputs(tparm(cursor,screenrow,screencol),1,char_to_terminal);
+      fprintf(out,"Incorrent choice,select again\n");
     }
   }while(!chosen);
+  tputs(clear,1,char_to_terminal);  
   return selected;
+}
+
+int char_to_terminal(int char_to_write)
+{
+  if(output_stream)
+    putc(char_to_write,output_stream);
+  return 0;
 }
