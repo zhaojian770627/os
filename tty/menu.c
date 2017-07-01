94d1c06c38a62084f356ad4ec467edbe8028fcfc
zhaojian770627 <zhaojian770627@163.com>
2017-07-01 15:05:44 +0800

    up

--- tty/menu.c
+++ tty/menu.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <termios.h>
 
 char *menu[]={
   "a - add new record",
@@ -16,6 +17,7 @@ int main()
   int choice=0;
   FILE *input;
   FILE *output;
+  struct termios initial_settings,new_settings;
 
   if(!isatty(fileno(stdout))){
     fprintf(stderr,"You are not a terminal.OK.\n");
@@ -29,11 +31,22 @@ int main()
     exit(1);
   }
 
+  tcgetattr(fileno(input),&initial_settings);
+  new_settings=initial_settings;
+  new_settings.c_lflag &=~ICANON;
+  new_settings.c_lflag &=~ECHO;
+  new_settings.c_cc[VMIN]=1;
+  new_settings.c_cc[VTIME]=0;
+  if(tcsetattr(fileno(input),TCSANOW,&new_settings)!=0){
+    fprintf(stderr,"could not set attributes\n");
+  }
+
   do{
     choice=getchoice("Please select an action",menu,input,output);
     printf("You have chosen:%c\n",choice);
   }while(choice!='q');
-  return 0;
+  tcsetattr(fileno(input),TCSANOW,&initial_settings);
+  exit(0);
 }
 
 int getchoice(char *greet,char *choices[],FILE *in,FILE *out)
