e59652dbc01df07961e1fff5de1b0e8cd45e7a66
zhaojian770627 <zhaojian770627@163.com>
2017-07-04 10:20:36 +0800

    add

--- protected/13/mbr.asm
+++ protected/13/mbr.asm
@@ -15,6 +15,18 @@
 	mov	ds,eax		;令DS指向该段以进行操作
 	mov	ebx,edx		;段内起始偏移地址
 
-	
+	;; 跳过0#描述符的槽位
+	;; 创建1#描述符,这是一个数据段，对应0~4GB的线性地址空间
+	mov	dword[ebx+0x08],0x0000ffff ;基地址为0，段界限位0xffff
+	mov	dword[ebx+0x0c],0x00cf9200 ;粒度位4KB，存储器段描述符
 
+	;; 创建保护模式下初始代码段描述符
+	mov	dword[ebx+0x10],0x7c0001ff ;基地址为0x00007c00，段界限位0x1ff
+	mov	dword[ebx+0x14],0x00489800 ;粒度位1个字节，代码段描述符
+
+	;; 创建保护模式下堆栈段描述符
+	mov	dword[ebx+0x18],0x7c00fffe ;基地址为0x00007c00，段界限位0xffffe
+	mov	dword[ebx+0x1c],0x00cf9600 ;粒度为4KB
+
+	
 	
