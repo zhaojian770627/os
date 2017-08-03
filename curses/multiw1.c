ee4bdf5fa19eccc59aa1979bf56e6f6320562f83
zhaojian770627 <zhaojian770627@163.com>
2017-08-03 23:06:09 +0800

    up

--- curses/multiw1.c
+++ curses/multiw1.c
@@ -26,6 +26,35 @@ int main()
   refresh();
   sleep(2);
 
+  new_window_ptr=newwin(10,20,5,5);
+  mvwprintw(new_window_ptr,2,2,"%s","Hello World");
+  mvwprintw(new_window_ptr,5,2,"%s","Notice how very long lines wrap inside the window");
+  wrefresh(new_window_ptr);
+  sleep(2);
+
+  a_letter='0';
+  for(y_loop=0;y_loop<LINES-1;y_loop++){
+    for(x_loop=0;x_loop<COLS-1;x_loop++){
+      mvwaddch(stdscr,y_loop,x_loop,a_letter);
+      a_letter++;
+      if(a_letter>'9')
+	a_letter='0';
+    }
+  }
+  refresh();
+  sleep(2);
+
+  touchwin(new_window_ptr);
+  wrefresh(new_window_ptr);
+  sleep(2);
+
+  popup_window_ptr=newwin(10,20,8,8);
+  box(popup_window_ptr,'|','-');
+  mvwprintw(popup_window_ptr,5,2,"%s","Pop Up Window!");
+  wrefresh(popup_window_ptr);
+  sleep(2);
+
+
   endwin();
   exit(EXIT_SUCCESS);
 }
