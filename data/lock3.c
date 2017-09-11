3ac6335fb5b07697ff1ba221cb542dd16c8a1a75
zhaojianc <zhaojianc@yonyou.com>
2017-09-11 13:45:18 +0800

    up

--- data/lock3.c
+++ data/lock3.c
@@ -14,3 +14,11 @@ int main()
   struct flock region_2;
   int res;
 
+  file_desc=open(test_file,O_RDWR|O_CREAT,0666);
+  if(!file_desc){
+    fprintf(stderr,"Unable to open %s for ead/write\n",test_file);
+    exit(EXIT_FAILURE);
+  }
+  for(byte_count=0;byte_count<100;byte_count++){
+    write(file_desc,byte_to_write,1);
+  }
