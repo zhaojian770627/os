ad6d722000c08ce65938eeef7493b306d79ddb02
zhaojianc <zhaojianc@yonyou.com>
2017-09-15 16:15:31 +0800

    add

--- proc/pexec.c
+++ proc/pexec.c
@@ -2,8 +2,8 @@
 #include <stdio.h>
 
 int main(){
-  printf("Running ps with system\n");
-  system("ps -ax &");
+  printf("Running ps with execlp\n");
+  execlp("ps","ps","-ax",0);
   printf("Done.\n");
   exit(0);
 }
