9d54ae69f3ba0c78fa3f3fd8383cfc11b5bceeaf
zhaojian770627 <zhaojian770627@163.com>
2017-10-15 22:33:56 +0800

    up

--- tcp/lib/unp.h
+++ tcp/lib/unp.h
@@ -6,6 +6,8 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 
+#include <time.h>
+
 #include <netinet/in.h>	/* sockaddr_in{} and other Internet defns */
 #include <errno.h>
 #include <stdio.h>
@@ -15,8 +17,20 @@
 /* Miscellaneous constants */
 #define	MAXLINE		4096	/* max text line length *
 
+/* Following could be derived from SOMAXCONN in <sys/socket.h>, but many
+   kernels still #define it as 5, while actually supporting many more */
+#define	LISTENQ		1024	/* 2nd argument to listen() */
+
 /* Following shortens all the typecasts of pointer arguments: */
 #define	SA	struct sockaddr
+
+
+int	 Socket(int, int, int);
+void	 Bind(int, const SA *, socklen_t);
+void	 Listen(int, int);
+int	 Accept(int, SA *, socklen_t *);
+void	 Write(int, void *, size_t);
+void	 Close(int);
 
 void	 err_dump(const char *, ...);
 void	 err_msg(const char *, ...);
