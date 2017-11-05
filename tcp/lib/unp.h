41bc6a796df2414f03257aedf7b02f0af347b929
zhaojian770627 <zhaojian770627@163.com>
2017-11-05 22:31:00 +0800

    add

--- tcp/lib/unp.h
+++ tcp/lib/unp.h
@@ -32,6 +32,13 @@
 
 ssize_t	 writen(int, const void *, size_t);
 
+/* prototypes for our stdio wrapper functions: see {Sec errors} */
+void	 Fclose(FILE *);
+FILE	*Fdopen(int, const char *);
+char	*Fgets(char *, int, FILE *);
+FILE	*Fopen(const char *, const char *);
+void	 Fputs(const char *, FILE *);
+
 const char *Inet_ntop(int, const void *, char *, size_t);
 
 
