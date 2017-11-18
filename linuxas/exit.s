911d82dca57c4f04e3f18d3f05fc506499bb3f0b
zhaojian770627 <zhaojian770627@163.com>
2017-11-18 21:11:38 +0800

    add

---
+++ linuxas/exit.s
@@ -0,0 +1,13 @@
+# 目的:退出并向Linux内核返回一个状态码的简单程序
+# 输入:无
+# 输出:返回一个状态码。在运行程序后可通过输入echo $?来读取状态码
+# 变量:
+# 	%eax保存系统调用号
+# 	%ebx保存返回状态
+	.section .text
+	.global _start
+_start:
+	movl	$1,%eax
+	movl	$0,%ebx
+	int	$0x80
+	
