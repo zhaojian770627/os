b3c5c5179aefacd57b2900851e72ddbc1f44e4a2
zhaojian770627 <zhaojian770627@163.com>
2017-11-21 23:43:26 +0800

    add

--- linuxas/factor.s
+++ linuxas/factor.s
@@ -22,6 +22,17 @@ factorial:
 	movl	8(%ebp),%eax
 	cmpl	$1,%eax
 	je	end_factiorial
+
 	decl	%eax
 	pushl	%eax
+	call	factorial
+	;; 此句可加可不加，因为ebp已恢复
+	addl	$4,%esp
+	
+	movl	8(%ebp),%ebx
+	imull	%ebx,%eax
+end_factiorial:
+	movl	%ebp,%esp
+	popl	%ebp
+	ret
 	
