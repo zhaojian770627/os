042b83429fc38d12f07d7b85981ce03ca107532c
zhaojianc <zhaojianc@yonyou.com>
2017-11-21 20:43:31 +0800

    add

--- linuxas/factor.s
+++ linuxas/factor.s
@@ -4,3 +4,24 @@
 
 	.globl _start
 	.globl factorial
+
+_start:
+	pushl	$4
+	call	factorial
+	addl	$4,%esp
+	movl	%eax,%ebx
+
+	movl	$1,%eax
+	int	$0x80
+
+	.type factorial,@function
+factorial:
+	pushl	%ebp
+	mov	%esp,%ebp
+
+	movl	8(%ebp),%eax
+	cmpl	$1,%eax
+	je	end_factiorial
+	decl	%eax
+	pushl	%eax
+	
