901a382569088326b8c49d167a078176730ff688
zhaojian770627 <zhaojian770627@163.com>
2017-12-10 21:57:25 +0800

    up

--- tcp/tcpcliserv/tcpservselect01.c
+++ tcp/tcpcliserv/tcpservselect01.c
@@ -28,4 +28,50 @@ main(int argc,char **argv)
     client[i]=-1;		/* -1 indicates available entry */
   FD_ZERO(&allset);
   FD_SET(listenfd,&allset);
+
+  for(;;){
+    rset=allset;		/* structure assignment */
+    nready=Select(maxfd+1,&rset,NULL,NULL,NULL);
+
+    if(FD_ISSET(listenfd,&rset)){ /* now client connection */
+      clilen=sizeof(cliaddr);
+      connfd=Accept(listenfd,(SA*)&cliaddr,&clilen);
+
+      for(i=0;i<FD_SETSIZE;i++)
+	if(client[i]<0){
+	  client[i]=connfd;	/* save descriptor */
+	  break;
+	}
+      if(i==FD_SETSIZE)
+	err_quit("too many clients");
+
+      FD_SET(connfd,&allset);	/* add new descriptor to set */
+      if(connfd>maxfd)
+	maxfd=connfd;		/* for select */
+
+      if(i>maxi)
+	maxi=i;			/* max index in client[] array */
+
+      if(--nready<=0)
+	continue;		/* no more readable descriptors */
+    }
+
+    for(i=0;i<=maxi;i++){	/* check all clients for data */
+      if((sockfd=client[i])<0)
+	continue;
+
+      if(FD_ISSET(sockfd,&rset)){
+	if((n=Read(sockfd,buf,MAXLINE))==0){
+	  /* connection closed by client */
+	  Close(sockfd);
+	  FD_CLR(sockfd,&allset);
+	  client[i]=-1;
+	}else
+	  Write(sockfd,buf,n);
+
+	if(--nready<=0)
+	  break;			/* no more readable descriptors */
+      }
+    }
+  }
 }
