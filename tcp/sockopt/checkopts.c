51c2658b1e5214d79d794640ef3308aa22d9dc9b
zhaojianc <zhaojianc@yonyou.com>
2017-12-20 10:39:04 +0800

    add

--- tcp/sockopt/checkopts.c
+++ tcp/sockopt/checkopts.c
@@ -3,14 +3,57 @@
 #include	"../lib/unp.h"
 #include	<netinet/tcp.h>		/* for TCP_xxx defines */
 
+union val {
+  int				i_val;
+  long				l_val;
+  struct linger		linger_val;
+  struct timeval	timeval_val;
+} val;
 
-/* *INDENT-ON* */
-/* end checkopts1 */
+static char	*sock_str_flag(union val *, int);
+static char	*sock_str_int(union val *, int);
+static char	*sock_str_linger(union val *, int);
+static char	*sock_str_timeval(union val *, int);
 
-/* include checkopts2 */
+struct sock_opts {
+  const char	   *opt_str;
+  int		opt_level;
+  int		opt_name;
+  char   *(*opt_val_str)(union val *, int);
+} sock_opts[] = {
+  { "SO_BROADCAST",		SOL_SOCKET,	SO_BROADCAST,	sock_str_flag }
+};
+
+
+  /* *INDENT-ON* */
+  /* end checkopts1 */
+
+  /* include checkopts2 */
 int
 main(int argc, char **argv)
 {
+  int		   	fd;
+  socklen_t		len;
+  struct sock_opts	*ptr;
 
+  for (ptr = sock_opts; ptr->opt_str != NULL; ptr++) {
+  }
   exit(0);
+}
+
+
+/* include checkopts3 */
+static char	strres[128];
+
+static char	*
+sock_str_flag(union val *ptr, int len)
+{
+  /* *INDENT-OFF* */
+  if (len != sizeof(int))
+    snprintf(strres, sizeof(strres), "size (%d) not sizeof(int)", len);
+  else
+    snprintf(strres, sizeof(strres),
+	     "%s", (ptr->i_val == 0) ? "off" : "on");
+  return(strres);
+  /* *INDENT-ON* */
 }
