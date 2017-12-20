3a32c39a1e884a861abda8164bbc4bba8c0dd6b2
zhaojianc <zhaojianc@yonyou.com>
2017-12-20 10:31:30 +0800

    add

---
+++ tcp/sockopt/checkopts.c
@@ -0,0 +1,16 @@
+/* include checkopts1 */
+/* *INDENT-OFF* */
+#include	"../lib/unp.h"
+#include	<netinet/tcp.h>		/* for TCP_xxx defines */
+
+
+/* *INDENT-ON* */
+/* end checkopts1 */
+
+/* include checkopts2 */
+int
+main(int argc, char **argv)
+{
+
+  exit(0);
+}
