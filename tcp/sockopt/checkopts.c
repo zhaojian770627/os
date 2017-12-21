dd8aec2a6332042252485a030d0f2e7a6b6eb336
zhaojianc <zhaojianc@yonyou.com>
2017-12-21 11:27:24 +0800

    add

--- tcp/sockopt/checkopts.c
+++ tcp/sockopt/checkopts.c
@@ -21,7 +21,17 @@ struct sock_opts {
   int		opt_name;
   char   *(*opt_val_str)(union val *, int);
 } sock_opts[] = {
-  { "SO_BROADCAST",		SOL_SOCKET,	SO_BROADCAST,	sock_str_flag }
+  { "SO_BROADCAST",		SOL_SOCKET,	SO_BROADCAST,	sock_str_flag },
+  { "SO_DEBUG",			SOL_SOCKET,	SO_DEBUG,	sock_str_flag },
+  { "SO_DONTROUTE",		SOL_SOCKET,	SO_DONTROUTE,	sock_str_flag },
+  { "SO_ERROR",			SOL_SOCKET,	SO_ERROR,	sock_str_int },
+  { "SO_KEEPALIVE",		SOL_SOCKET,	SO_KEEPALIVE,	sock_str_flag },
+  { "SO_LINGER",		SOL_SOCKET,	SO_LINGER,	sock_str_linger },
+  { "SO_OOBINLINE",		SOL_SOCKET,	SO_OOBINLINE,	sock_str_flag },
+  { "SO_RCVBUF",		SOL_SOCKET,	SO_RCVBUF,	sock_str_int },
+{ "SO_SNDBUF",			SOL_SOCKET,	SO_SNDBUF,	sock_str_int },
+
+  { NULL,			0,		0,		NULL }
 };
 
 
@@ -37,6 +47,38 @@ main(int argc, char **argv)
   struct sock_opts	*ptr;
 
   for (ptr = sock_opts; ptr->opt_str != NULL; ptr++) {
+    printf("%s: ", ptr->opt_str);
+    if (ptr->opt_val_str == NULL)
+      printf("(undefined)\n");
+    else{
+      switch(ptr->opt_level) {
+      case SOL_SOCKET:
+      case IPPROTO_IP:
+      case IPPROTO_TCP:
+	fd = Socket(AF_INET, SOCK_STREAM, 0);
+	break;
+#ifdef	IPV6
+      case IPPROTO_IPV6:
+	fd = Socket(AF_INET6, SOCK_STREAM, 0);
+	break;
+#endif
+#ifdef	IPPROTO_SCTP
+      case IPPROTO_SCTP:
+	fd = Socket(AF_INET, SOCK_SEQPACKET, IPPROTO_SCTP);
+	break;
+#endif
+      default:
+	err_quit("Can't create fd for level %d\n", ptr->opt_level);
+      }
+      len = sizeof(val);
+      if (getsockopt(fd, ptr->opt_level, ptr->opt_name,
+		     &val, &len) == -1) {
+	err_ret("getsockopt error");
+      } else {
+	printf("default = %s\n", (*ptr->opt_val_str)(&val, len));
+      }
+      close(fd);
+    }
   }
   exit(0);
 }
@@ -56,4 +98,28 @@ sock_str_flag(union val *ptr, int len)
 	     "%s", (ptr->i_val == 0) ? "off" : "on");
   return(strres);
   /* *INDENT-ON* */
+}
+
+static char	*
+sock_str_int(union val *ptr, int len)
+{
+	if (len != sizeof(int))
+		snprintf(strres, sizeof(strres), "size (%d) not sizeof(int)", len);
+	else
+		snprintf(strres, sizeof(strres), "%d", ptr->i_val);
+	return(strres);
+}
+
+static char	*
+sock_str_linger(union val *ptr, int len)
+{
+  struct linger	*lptr = &ptr->linger_val;
+
+  if (len != sizeof(struct linger))
+    snprintf(strres, sizeof(strres),
+	     "size (%d) not sizeof(struct linger)", len);
+  else
+    snprintf(strres, sizeof(strres), "l_onoff = %d, l_linger = %d",
+	     lptr->l_onoff, lptr->l_linger);
+  return(strres);
 }
