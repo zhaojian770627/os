029d5df82bd43c8858a98ce5e5ba167320f3ae9a
zhaojianc <zhaojianc@yonyou.com>
2017-12-26 20:57:30 +0800

    add

--- minix/boot/masterboot.s
+++ minix/boot/masterboot.s
@@ -1,19 +1,21 @@
-	LOADOFF	   =	0x7C00	# 0x0000:LOADOFF is where this code is loaded
-	BUFFER	   =	0x0600	# First free memory
-	PART_TABLE =	   446	# Location of partition table within this code
-	PENTRYSIZE =	    16	# Size of one partition table entry
-	MAGIC	   =	   510	# Location of the AA55 magic number
+	LOADOFF	   equ	0x7C00	;0x0000:LOADOFF is where this code is loaded 
+	BUFFER	   equ	0x0600	;First free memory 
+	PART_TABLE equ	   446	;Location of partition table within this code
+	PENTRYSIZE equ	    16	;Size of one partition table entry
+	MAGIC	   equ	   510	;Location of the AA55 magic number
 
 
-	bootind	   =	     0
-	sysind	   =	     4
-	lowsec	   =	     8
+	bootind	   equ	     0
+	sysind	   equ	     4
+	lowsec	   equ	     8
+[section .text]
 master:
 	xor	ax, ax
 	mov	ds, ax
 	mov	es, ax
 	cli
-	mov	ss, ax			! ds = es = ss = Vector segment
-	mov	sp, #LOADOFF
+	mov	ss, ax			;ds = es = ss = Vector segment
+	mov	sp, LOADOFF
 	sti
+	;; Copy this code to safety, then jump to it.
 	
