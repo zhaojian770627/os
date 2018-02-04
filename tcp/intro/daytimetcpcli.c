f942f8cfb14ff8b6c3d8586a357f26fbab720865
zhaojian770627 <zhaojian770627@163.com>
2018-02-04 19:51:25 +0800

    add

--- tcp/intro/daytimetcpcli.c
+++ tcp/intro/daytimetcpcli.c
@@ -18,7 +18,7 @@ main(int argc,char **argv)
 
   bzero(&servaddr,sizeof(servaddr));
   servaddr.sin_family=AF_INET;
-  servaddr.sin_port=htons(13);
+  servaddr.sin_port=htons(9999);
   if(inet_pton(AF_INET,argv[1],&servaddr.sin_addr)<=0)
     err_sys("inet_pton error for %s",argv[1]);
 
