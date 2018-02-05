1025de078b13c4c8e6bb1c04c208d5523fcefa61
zhaojian770627 <zhaojian770627@163.com>
2018-02-05 21:43:58 +0800

    add

---
+++ tcp/lib/connect_timeo.c
@@ -0,0 +1,30 @@
+#include "unp.h"
+
+static void connect_alarm(int);
+
+int
+connect_timeo(int sockfd,const SA *saptr,socklen_t salen,int nsec)
+{
+  Sigfunc *sigfunc;
+  int n;
+
+  sigfunc=Signal(SIGALRM,connect_alarm);
+  if(alarm(nsec)!=0)
+    err_msg("connect_timeo:alarm was already set");
+
+  if((n=connect(sockfd,saptr,salen))<0){
+    close(sockfd);
+    if(errno==ENITR)
+      errno=ETIMEDOUT;
+  }
+  alarm(0);			/* turn off the alarm */
+  Signal(SIGALRM,sigfunc);	/* restore previous signal handler */
+
+  return n;
+}
+
+static void
+connect_alarm(int signo)
+{
+  return;			/* just interrupt the connect() */
+}
